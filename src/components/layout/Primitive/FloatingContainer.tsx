c8846a7e12a604d369ba16224a9414de9dab89df
Abhinav <abhinav.ranger@gmail.com>
2026-03-24 19:57:23 +0530

    update: simplify the component

--- src/components/layout/Primitive/FloatingContainer.tsx
+++ src/components/layout/Primitive/FloatingContainer.tsx
@@ -2,13 +2,11 @@ import { forwardRef } from "react"
 import { cn } from "@/lib"
 
 export interface FloatingContainerProps extends React.HTMLAttributes<HTMLDivElement> {
-    edge?: "top" | "bottom" | "left" | "right";
     background?: "blur" | "opaque" | "transparent";
     gap?: number;
     align?: "start" | "center" | "end";
     justify?: "start" | "center" | "end";
     zIndex?: string;
-    bordered?: boolean;
     padding?: string;
     distanceFromEdge?: number;
 }
@@ -16,7 +14,6 @@ export interface FloatingContainerProps extends React.HTMLAttributes<HTMLDivElem
 export const FloatingContainer = forwardRef<HTMLDivElement, FloatingContainerProps>(
     (
         {
-            edge = "bottom",
             background = "blur",
             gap = 2,
             align,
@@ -30,22 +27,14 @@ export const FloatingContainer = forwardRef<HTMLDivElement, FloatingContainerPro
         }: FloatingContainerProps,
         ref: React.Ref<HTMLDivElement>
     ) => {
-        const isHorizontal = edge === "top" || edge === "bottom"
 
         return (
             <div
                 ref={ref}
                 className={cn(
-                    "absolute flex",
+                    "flex sticky rounded-lg",
 
-                    // edge positioning
-                    edge === "top" && `top-${distanceFromEdge} left-0 right-0`,
-                    edge === "bottom" && `bottom-${distanceFromEdge} left-0 right-0`,
-                    edge === "left" && `left-${distanceFromEdge} top-0 bottom-0`,
-                    edge === "right" && `right-${distanceFromEdge} top-0 bottom-0`,
-
-                    // direction
-                    isHorizontal ? "flex-row" : "flex-col",
+                    `bottom-${distanceFromEdge} left-0 right-0`,
 
                     // background
                     background === "blur" && "backdrop-blur bg-(--surface-elevated)/70",
