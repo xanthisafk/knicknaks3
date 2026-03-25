2d0a3dfbc25a43606e1983cfca240b82d75674e1
Abhinav <abhinav.ranger@gmail.com>
2026-03-26 01:23:13 +0530

    update: minor visual tweaks

--- src/components/ui/ExpectContent.tsx
+++ src/components/ui/ExpectContent.tsx
@@ -5,9 +5,9 @@ interface ExpectContentProps {
     emoji?: string;
 }
 
-export function ExpectContent({ text, emoji = "🪀" }: ExpectContentProps) {
+export function ExpectContent({ text = "Results will show up here", emoji = "🪀" }: ExpectContentProps) {
     return (
-        <div className="w-full grid grid-cols-1 p-6 gap-12 justify-items-center">
+        <div className="w-full h-full flex flex-col items-center justify-center p-6 gap-12">
             <span className="font-emoji text-6xl">{emoji}</span>
             <Label>{text}</Label>
         </div>
