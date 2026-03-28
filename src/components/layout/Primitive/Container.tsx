2cfd18ea6e3bf8edfe4ecabec0e5bd5ca4e8d5d8
Abhinav <abhinav.ranger@gmail.com>
2026-03-29 01:03:36 +0530

    update: custom col props

--- src/components/layout/Primitive/Container.tsx
+++ src/components/layout/Primitive/Container.tsx
@@ -3,41 +3,63 @@ import { cn } from "@/lib/utils"
 
 export interface ContainerProps
     extends React.HTMLAttributes<HTMLDivElement> {
-    mobileCols?: number;
-    cols?: number;
-    gap?: number;
+    mobileCols?: number
+    cols?: number
+    gap?: number
+    customCols?: string
+    mdCustomCols?: string
 }
 
-export const Container = React.forwardRef<HTMLDivElement, ContainerProps>(({
-    mobileCols = 1,
-    cols = 1,
-    gap = 2,
-    className,
-    ...props
-}, ref) => {
-    if (cols <= 0) cols = 1;
-    if (cols > 12) cols = 12;
-    if (mobileCols <= 0) mobileCols = 1;
-    if (mobileCols > 12) mobileCols = 12;
-    if (gap <= 0) gap = 2;
-    const gapClass = typeof gap === "number" ? `gap-${gap}` : undefined;
-    const colsClass = typeof cols === "number" ? `md:grid-cols-${cols}` : undefined;
-    const mobileColsClass = typeof mobileCols === "number" ? `grid-cols-${mobileCols}` : undefined;
+export const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
+    (
+        {
+            mobileCols = 1,
+            cols = 1,
+            gap = 2,
+            customCols,
+            mdCustomCols,
+            className,
+            style,
+            ...props
+        },
+        ref
+    ) => {
+        // sanitize
+        if (cols <= 0) cols = 1
+        if (cols > 12) cols = 12
+        if (mobileCols <= 0) mobileCols = 1
+        if (mobileCols > 12) mobileCols = 12
+        if (gap <= 0) gap = 2
 
-    return (
-        <div
-            ref={ref}
-            className={cn(
-                "grid",
-                gapClass,
-                mobileColsClass,
-                colsClass,
-                className
-            )}
-            {...props}
-        />
-    )
-}
+        const gapClass = `gap-${gap}`
+
+        const colsClass = !customCols ? `md:grid-cols-${cols}` : undefined
+        const mobileColsClass = !customCols
+            ? `grid-cols-${mobileCols}`
+            : undefined
+
+        const gridStyles: React.CSSProperties = {
+            ...style,
+            ...(customCols && {
+                gridTemplateColumns: customCols,
+            }),
+        }
+
+        return (
+            <div
+                ref={ref}
+                className={cn(
+                    "grid",
+                    gapClass,
+                    mobileColsClass,
+                    colsClass,
+                    className
+                )}
+                style={gridStyles}
+                {...props}
+            />
+        )
+    }
 )
 
 Container.displayName = "Container"
